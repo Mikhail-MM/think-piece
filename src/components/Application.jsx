94f5b6d738364d197ff91a1ebe807f111df22a82
Mikhail Maslyuk <leakymee@bu.edu>
2019-08-01 13:56:59 -0400

    Initialize basic firestore query for posts

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { firestore } from '../firebase';
+
 import Posts from './Posts';
 
 class Application extends Component {
@@ -35,6 +37,10 @@ class Application extends Component {
       },
     ],
   };
+
+  componentDidMount = () => {
+    const posts = firestore.collection('posts').get();
+  }
 
   handleCreate = post => {
     const { posts } = this.state;
---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var config = {
+  apiKey: "AIzaSyBSrxwqn9FQ91WRjPqqQVpQghZpE5zD00w",
+  authDomain: "react-firebase-blogger.firebaseapp.com",
+  databaseURL: "https://react-firebase-blogger.firebaseio.com",
+  projectId: "react-firebase-blogger",
+  storageBucket: "gs://react-firebase-blogger.appspot.com/",
+  messagingSenderId: "889931848424",
+  appId: "1:889931848424:web:73fb709cf4551af4"
+};
+
+/*This configuration "settings" object is used because FireBase added a breaking change
+* to how dates were stored internally.
+*/
+
+
+
+if (process.env.NODE_ENV === 'development') {
+  // Add firebase object to global window for experimentation
+  window.firebase = firebase;
+}
+
+firebase.initializeApp(config);
+
+export const firestore = firebase.firestore();
+
+const settings = {
+  timestampsInSnapshots: true
+};
+
+firestore.settings(settings);
